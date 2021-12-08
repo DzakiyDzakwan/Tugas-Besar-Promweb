e50737524e81fa0fb7571f913b85ecb96be0f32f
Dzakiy Dzakwan <dzakcart@gmail.com>
2021-12-08 15:40:27 +0700

    pagination -Jaki

--- php/pagination.php
+++ php/pagination.php
@@ -1,12 +1,12 @@
 <div class="my-3">
     <nav aria-label="Page navigation example" class="mx-auto new-pagination">
     <ul class="pagination">
-        <?php if($halamanAktif != 1) : ?>
-        <li class="page-item">
-        <a class="page-link" href="?page<?=$j - 1?>" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-        </a>
-        </li>
+        <?php if($halamanAktif > 1) : ?>
+            <li class="page-item">
+                <a class="page-link" href="?page=<?=$halamanAktif - 1?>" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
         <?php endif ; ?>
               
         <?php for($j=1; $j <= $jumlahHalaman; $j++) : ?>
@@ -18,13 +18,13 @@
         <?php endif ; ?>
 
         <?php endfor ; ?>
-
-        <?php if($halamanAktif != 1) : ?>
-        <li class="page-item">
-        <a class="page-link" href="?page<?=$j + 1?>" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-        </a>
-        </li>
+        
+        <?php if($halamanAktif < $jumlahHalaman) : ?>
+            <li class="page-item">
+                <a class="page-link" href="?page=<?=$halamanAktif + 1?>" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
         <?php endif ; ?>
     </ul>
     </nav>
