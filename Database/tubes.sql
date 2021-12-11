fd035f1d3fb3a756461ce9cdc7a139899321bef5
Dzakiy Dzakwan <dzakcart@gmail.com>
2021-12-11 07:07:30 +0700

    database update

--- Database/tubes.sql
+++ Database/tubes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2021 at 03:25 AM
+-- Generation Time: Dec 11, 2021 at 01:06 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -40,10 +40,11 @@ CREATE TABLE `guru` (
 --
 
 INSERT INTO `guru` (`id`, `nama_guru`, `NIG`, `mapel_id`, `user_id`) VALUES
-(5, 'Guru 1', '190123001', 1, 10),
-(6, 'Guru 2', '190121002', 3, 11),
-(7, 'Guru3', '190121003', 3, 12),
-(8, 'Guru 4', '190121004', 2, 13);
+(9, 'Ahmad Surtana', '191102001', 5, 26),
+(10, 'Maulida Yatna', '191102002', 3, 23),
+(11, 'Zulaika Hana', '191102003', 6, 24),
+(12, 'Efendi Ginting', '191102004', 5, 25),
+(13, 'Zainal Arifin', '191102005', 2, 27);
 
 -- --------------------------------------------------------
 
@@ -65,7 +66,8 @@ CREATE TABLE `jawaban` (
 --
 
 INSERT INTO `jawaban` (`id`, `jawaban`, `nilai`, `siswa`, `tugas`, `mapel`) VALUES
-(1, 'E-Commerce.pdf', 80, 4, 2, 3);
+(7, 'UTS - PemrogBerorientasiObjek-S1TIA_2020.pdf', 100, 9, 6, 5),
+(8, 'JADWAL UAS SEMESTER GANJIL 2021-2022.pdf', 80, 11, 6, 5);
 
 -- --------------------------------------------------------
 
@@ -85,10 +87,10 @@ CREATE TABLE `kelas` (
 --
 
 INSERT INTO `kelas` (`id`, `nama_kelas`, `jurusan`, `wali_kelas`) VALUES
-(2, '10 A', 'IPA', 5),
-(3, '11 A', 'IPA', 6),
-(4, '12 A', 'IPA', 7),
-(5, '12 B', 'IPS', 8);
+(7, '10 A', 'IPA', 9),
+(8, '10 A', 'IPS', 10),
+(9, '11 A', 'IPA', 12),
+(10, '11 A', 'IPS', 11);
 
 -- --------------------------------------------------------
 
@@ -109,7 +111,9 @@ CREATE TABLE `mapel` (
 INSERT INTO `mapel` (`id`, `nama_mapel`, `Created_at`) VALUES
 (1, 'Matematika Wajib', '2021-11-13 02:28:45'),
 (2, 'B. Indonesia', '2021-11-13 02:50:46'),
-(3, 'Matematika Peminatan', '2021-11-13 17:17:56');
+(3, 'Matematika Peminatan', '2021-11-13 17:17:56'),
+(5, 'Fisika', '2021-12-10 05:03:08'),
+(6, 'Kimia', '2021-12-10 05:03:11');
 
 -- --------------------------------------------------------
 
@@ -128,10 +132,7 @@ CREATE TABLE `mapel_kelas` (
 --
 
 INSERT INTO `mapel_kelas` (`id`, `kelas`, `guru`) VALUES
-(1, 2, 6),
-(2, 2, 8),
-(3, 5, 6),
-(5, 4, 6);
+(7, 7, 9);
 
 -- --------------------------------------------------------
 
@@ -152,9 +153,11 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`id`, `nama_siswa`, `NIS`, `kelas_id`, `user_id`) VALUES
-(3, 'Siswa 4', '211121004', 2, 7),
-(4, 'Siswa 2', '211121002', 2, 8),
-(5, 'Siswa 3', '211121003', 5, 9);
+(9, 'Ayash Batubara', '211402001', 7, 19),
+(10, 'Ahmad Budiawan', '211402002', 8, 20),
+(11, 'Reisa Ananda', '211402003', 7, 21),
+(12, 'Ilham Kurniawan', '211302001', 8, 28),
+(13, 'Ayu Nabila', '211302003', 8, 29);
 
 -- --------------------------------------------------------
 
@@ -177,8 +180,7 @@ CREATE TABLE `tugas` (
 --
 
 INSERT INTO `tugas` (`id`, `nama_tugas`, `deskripsi`, `deadline`, `created_at`, `guru`, `kelas`) VALUES
-(2, 'E-Commerce', '<p><strong>Buatlah</strong></p><ul><li>makalah tentang e-commerce</li><li>Buat PPT nya</li></ul>', '2021-12-25', '2021-12-02 09:57:52', 6, 2),
-(3, 'Quiz', '<p>Dengan Kelompok yang sudah dibagikan kemarin. Buatlah :</p><ol><li>Mindmapping</li><li>Daftar Tugas</li><li>Form</li></ol><p>Dikumpul dalam bentuk PDF</p>', '2021-12-04', '2021-12-02 15:44:10', 6, 2);
+(6, 'Hukum Archimedes dan Pascal', '<p>Buatlah <strong>Rangkuman</strong> tentang <strong>HUKUM ARCHIMEDES</strong>&nbsp;</p><p>Buatlah rangkuman dalam bentuk <strong>PDF</strong></p>', '2021-12-24', '2021-12-10 07:12:06', 9, 7);
 
 -- --------------------------------------------------------
 
@@ -202,13 +204,18 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `status`, `Created_at`) VALUES
 (1, 'admin', '$2y$10$YUgrqufmksozuj1VtDN1C.8DwEPPBOx3AMT5yFCeYCUsResux2qBq', 'admin@gmail.com', 'ADMIN', '2021-11-13 02:28:01'),
 (6, 'admin3', '$2y$10$7QGWGfFoBpnO/0kF79LQ3ulVtr0FKJPh77PKbN7RzumVlxnwsIW.e', 'admin3@gmail.com', 'ADMIN', '2021-11-18 02:41:54'),
-(7, 'siswa4', '$2y$10$9DeaZ/QWJA7OKNcpLvZ82.zLCIRk0Cb3vPeExoSq2ShHmOvpMQ09e', 'siswa4@gmail.com', 'SISWA', '2021-11-20 03:30:49'),
-(8, 'siswa2', '$2y$10$flZcNQKe0n0dN01V.tSHh.Y.4oHS9HNWwc219DO5y2jOmNaC37VG.', 'siswa2@gmail.com', 'SISWA', '2021-11-20 03:31:01'),
-(9, 'siswa3', '$2y$10$7j8uWayxjYTuOwsJDJpNAOwZ3zfn5XB534S4jnQ8mple0chX0J8u.', 'siswa3@gmail.com', 'SISWA', '2021-11-20 03:31:23'),
-(10, 'guru1', '$2y$10$ppGIBjiJknu.sDn.hHl66OCz9M/KmitNLt5nFYXXmPlJ4ijGbt2yi', 'guru1@gmail.com', 'GURU', '2021-11-20 03:31:40'),
-(11, 'guru2', '$2y$10$NK0X0ZL1eeikZTOGJPeNpeX2YeC6LKvbWXQ6nvHSH..kUg23zaXKe', 'guru2@gmail.com', 'GURU', '2021-11-20 03:31:55'),
-(12, 'guru3', '$2y$10$zdcUodI1eKn0JSDioMzrUuj2QcrPqAI4LMz1jZg25po0I7C2AUwaa', 'guru3@gmail.com', 'GURU', '2021-11-20 03:36:22'),
-(13, 'guru4', '$2y$10$U29xmEr.yfAf/kMwIFobzewtEX67/o/Yd2ZKs/7nX.hxGengTShWK', 'guru4@gmail.com', 'GURU', '2021-11-21 01:52:32');
+(17, 'admin2', '$2y$10$1T4FRmIgNEPdyj5kbrlD5O23xmk7iO/EZZnKISgM74s.ZlJWBlHSi', 'admin2@gmail.com', 'ADMIN', '2021-12-10 04:54:21'),
+(18, 'admin4', '$2y$10$QCjBinThkpsjZ7FaM04D7eztjyd3AlL3usKOfUFP2SdS4onihpMHy', 'admin4@gmail.com', 'ADMIN', '2021-12-10 04:54:59'),
+(19, 'siswa1', '$2y$10$B9B6hK8zKCG2fdEWRG.qWOVJNixNr0u1TYTkvoWfFxto7eKyiIx/C', 'siswa1@gmail.com', 'SISWA', '2021-12-10 04:56:15'),
+(20, 'siswa2', '$2y$10$ds0VimdlzoRdpnUtX0hLOuTwC6o2RCL0ZtM2OT4yNdwarAlU/oT2W', 'siswa2@gmail.com', 'SISWA', '2021-12-10 04:56:32'),
+(21, 'siswa3', '$2y$10$TSmVjlQVI8QwC0uM4/B86uv6uEfH8/cpTeweAQCc5WClTIrk8svoi', 'siswa3@gmail.com', 'SISWA', '2021-12-10 04:56:54'),
+(23, 'guru2', '$2y$10$FurySm4kfg4Ic1puyPsjKeWtBGMGit6tXuRXPdp9I8ZYLzPwsfBzC', 'guru2@gmail.com', 'GURU', '2021-12-10 05:00:01'),
+(24, 'guru3', '$2y$10$5jIzKaTz21xRHpv37cFhDOC9tBhsIbUNXj3FfAdIMOgjWQbQaeNyK', 'guru3@gmail.com', 'GURU', '2021-12-10 05:00:17'),
+(25, 'guru4', '$2y$10$TkFvPdWv0CBo1qLoH.1WheL/Hebtt1BRAiAp4dRkndj0GRlZQupMK', 'guru4@gmail.com', 'GURU', '2021-12-10 05:00:32'),
+(26, 'guru1', '$2y$10$4PsUfpTNUbobH4ur0liaH.MZt0nauZv1GPRgzZiAlf.JQWb2gaIXy', 'guru1@gmail.com', 'GURU', '2021-12-10 05:04:36'),
+(27, 'guru5', '$2y$10$9yBeBKDAM5IrNjbd8719VeoTDvaLtq7jC1cuboUN8Ru4VC1GzHulK', 'guru5@gmail.com', 'GURU', '2021-12-10 05:07:46'),
+(28, 'siswa4', '$2y$10$cptKIp2XXcSTmSDj8Bm78utwbjGwVrcJGQMeIKGmEW4h.I/4vOHsS', 'siswa4@gmail.com', 'SISWA', '2021-12-10 05:17:33'),
+(29, 'siswa5', '$2y$10$oyg78JO4xOtdXCxYqoZrYu69xLlAoWq/8hAJ7QwFyJauRYH0UzVu.', 'siswa5@gmail.com', 'SISWA', '2021-12-10 05:17:52');
 
 --
 -- Indexes for dumped tables
@@ -284,49 +291,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `jawaban`
 --
 ALTER TABLE `jawaban`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `mapel_kelas`
 --
 ALTER TABLE `mapel_kelas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tugas`
 --
 ALTER TABLE `tugas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
